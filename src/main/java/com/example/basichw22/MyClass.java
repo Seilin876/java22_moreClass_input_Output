224823589b3f661431eeae46d4525b271035f603
shanglin10 <a0926753805@gmail.com>
2019-11-08 18:20:45 +0800

    1081108update

---
+++ src/main/java/com/example/basichw22/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.basichw22;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main (String[] args ) {
+        Scanner employees = new Scanner(System.in);
+        employees servitor = new employees();
+        servitor.setServitorNumber();
+        servitor.setAllVariable();
+        servitor.calc();
+    }
+}
